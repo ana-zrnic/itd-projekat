fc1507b7768b36af00a429f971c389a378a5b708
ana-zrnic <85364876+ana-zrnic@users.noreply.github.com>
2021-06-04 20:15:04 +0200

    Delete test2.py

--- Projekat/test2.py
+++
@@ -1,4 +0,0 @@
-import os
-import subprocess
-
-subprocess.call(('cd ' + r"C:\Users\WIN10\Desktop\itd-angular\BEtest\angular-flask\dist\angular-flask" + ' &&' + ' xcopy ' + r"test.txt" + r" C:\Users\WIN10\Desktop\itd-angular\BEtest\static"), shell=True)
